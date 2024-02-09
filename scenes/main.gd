b7e6bf8772da4113f8a8cfe5372912b808502e7e
Renato Meyer <rockethiena@gmail.com>
2024-02-09 19:08:05 +0000

    feat: preguntas - CRT

--- scenes/main.gd
+++ scenes/main.gd
@@ -5,6 +5,9 @@ onready var labelPregunta = $fondo/Node2D/Label
 onready var pataco = $Kekos/AnimatedSprite
 onready var estrofa = $AnimationEstrofa
 
+onready var labelUNO = $fondo/A/Label
+onready var labelDOS = $fondo/B/Label
+onready var labelTRES = $fondo/C/Label
 
 var respuestaActual
 var seleccion = 1
@@ -44,29 +47,43 @@ func _empezar():
 	
 	match contador:
 		1:
-			_preguntaNueva("esta es la pregunta 1", "bluey", 2)
+			_preguntaNueva("¿Es el opio bueno?", "bluey", 2)
+			_prepararRespuestas("si", "no", "depende de la ocasión")
 		2:
-			_preguntaNueva("esta es la pregunta 2", "doraemon", 3)
+			_preguntaNueva("¿Cuál ha sido la mejor época del opio?", "doraemon", 3)
+			_prepararRespuestas("Está por llegar", "Ahora", "¡Nunca!")
 		3:
-			_preguntaNueva("esta es la pregunta 3", "sonic", 1)
+			_preguntaNueva("Cuando tu madre fume opio ¿Qué harás?", "sonic", 1)
+			_prepararRespuestas("Denunciarla a la policía", "Pedirle", "Contarlo en el recreo")
 		4:
-			_preguntaNueva("esta es la pregunta 4", "charmander", 3)
+			_preguntaNueva("¿Cómo se escribe '___' ?", "charmander", 3)
+			_prepararRespuestas("Opio", "Opio", "Apio")
 		5:
-			_preguntaNueva("esta es la pregunta 5", "estebanco", 2)
+			_preguntaNueva("¿De qué color era la ropa del líder del ejercito francés en la segunda guerra del opio en 1856", "estebanco", 2)
+			_prepararRespuestas("#008f39", "Azul, roja y blanca", "Verde opio")
 		6:
-			_preguntaNueva("esta es la pregunta 6", "pocoyo", 1)
+			_preguntaNueva("¿A cuánto está el gramo de opio?", "pocoyo", 1)
+			_prepararRespuestas("¡No quiero saberlo!", "34$", "El que sea necesario")
 		7:
-			_preguntaNueva("esta es la pregunta 7", "peppa", 1)
+			_preguntaNueva("¿Cómo prefieres el opio?", "peppa", 1)
+			_prepararRespuestas("Viéndolo (lejos)", "Viéndolo (cerca)", "Fumándolo")
 		8:
-			_preguntaNueva("esta es la pregunta 8", "bort", 3)
+			_preguntaNueva("¿Dónde guardaría tu tía Marietta el opio?", "bort", 3)
+			_prepararRespuestas("En el sujetador", "En la guantera", "Ya no es mi tía.")
 		9:
-			_preguntaNueva("esta es la pregunta 1", "eufrasio", 2)
+			_preguntaNueva("El 7 de Enero de 1945 ¿Quién dijo en el Condado de Boone, en Arkansas, en la calle Churchill, a las 22:31, vestido con un sombrero vaquero y una corbata roja, la siguiente frase: 'Me gusta el opio'?", "eufrasio", 2)
+			_prepararRespuestas("Una persona", "Un criminal", "Optimus Prime")
 		10:
-			_preguntaNueva("esta es la pregunta 1", "miki", 1)
+			_preguntaNueva("Pregunta trampa: ¿Es el opio bueno?", "miki", 2)
+			_prepararRespuestas("si", "no", "depende de la ocasión")
 	
-
+func _prepararRespuestas(uno,dos,tres):
+	labelUNO.text = uno
+	labelDOS.text = dos
+	labelTRES.text = tres
 	
 func _preguntaNueva(pregunta, keko, respuestaCorrecta):
+	labelPregunta.text = pregunta
 	respuestaActual = respuestaCorrecta
 	pataco.play(keko)
 	pataco.stop()
